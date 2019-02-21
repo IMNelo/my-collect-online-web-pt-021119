31b62f23a143c94ebe0d525a6da7424d45500c2d
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-21 09:38:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,13 @@
-#array = ["ruby", "javascript", "python", "objective-c"]
-
 def my_collect(array)
   counter = 0
-  collection = []
-    while counter < array.length
-  collection << yield(array[counter])
+    collection = []
+    while counter < array.length.upcase
+    collection << yield(array[counter])
   counter += 1
     end
-  collection
+    collection
 end
 
 
-my_collect(["ruby", "javascript", "python", "objective-c"]) do |lang| lang.upcase
+my_collection(["ruby", "javascript", "python", "objective-c"]) do |lang| lang.upcase
 end
